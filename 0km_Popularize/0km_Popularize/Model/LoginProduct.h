f1b3d23bea2c4ab90e0038def4053a9f079595a4
zhenghongchuan <ZhengHongchuan@zhenghongchuandeMacBook-Pro.local>
2014-08-16 18:29:39 +0800

    看不到文档...

---
+++ 0km_Popularize/0km_Popularize/Model/LoginProduct.h
@@ -0,0 +1,13 @@
+//
+//  LoginProduct.h
+//  0km_Popularize
+//
+//  Created by 郑鸿川 on 14/8/16.
+//  Copyright (c) 2014年 agile. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LoginProduct : NSObject
+
+@end
---
+++ 0km_Popularize/0km_Popularize/Model/LoginProduct.m
@@ -0,0 +1,13 @@
+//
+//  LoginProduct.m
+//  0km_Popularize
+//
+//  Created by 郑鸿川 on 14/8/16.
+//  Copyright (c) 2014年 agile. All rights reserved.
+//
+
+#import "LoginProduct.h"
+
+@implementation LoginProduct
+
+@end
